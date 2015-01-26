7275c92aedc2436da4db78f28b25f0dc1cfd9274
Visage <Visage@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-26 20:21:10 +0000

    Adding first version of Visage oEmbed
    git-svn-id: https://plugins.svn.wordpress.org/visage-oembed/trunk@1076231 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,22 @@
+=== Visage oEmbed ===
+Contributors: Drew Gourley, Visage
+Tags: oEmbed
+Requires at least: 3.5
+Tested up to: 4.1
+Stable tag: 0.1
+License: GPL2
+License URI: www.gnu.org/licenses/gpl-2.0.html
+
+This plugin adds Visage as an oEmbed provider.
+
+== Description ==
+This plugin adds Visage as an oEmbed provider so you can share charts in your blog by simply pasting the view URL into the content area of your blog posts.
+
+== Installation ==
+Simply install the plugin to your WordPress site, everything else is automatic.
+
+== Frequently Asked Questions ==
+No questions asked yet. You could be the first!
+
+== Changelog ==
+0.1: Plugin initial release.
---
+++ visage-oembed.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Plugin Name: Visage oEmbed
+ * Plugin URI: http://visage.co/visage-oembed-plugin
+ * Description: This plugin adds Visage as an oEmbed provider.
+ * Version: 0.1
+ * Author: Drew Gourley, Visage
+ * Author URI: http://visage.co
+ * License: GPL2
+ */
+
+/*  Copyright YEAR  PLUGIN_AUTHOR_NAME  (email : PLUGIN AUTHOR EMAIL)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+defined('ABSPATH') or die("Nope.");
+
+function visage_oembed_provider() {
+	wp_oembed_add_provider( 'http://gorgo.visage.co/chart/embed/*', 'http://gorgo.visage.co/oembed', false );
+	wp_oembed_add_provider( 'http://gorgo.visage.co/chart/view/*', 'http://gorgo.visage.co/oembed', false );
+
+	wp_oembed_add_provider( 'http://gorgo-staging.herokuapp.com/chart/embed/*', 'http://gorgo-staging.herokuapp.com/oembed', false );
+	wp_oembed_add_provider( 'http://gorgo-staging.herokuapp.com/chart/view/*', 'http://gorgo-staging.herokuapp.com/oembed', false );
+}
+add_action( 'init', 'visage_oembed_provider' );
+?>
